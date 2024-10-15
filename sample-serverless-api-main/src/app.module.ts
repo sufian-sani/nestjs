b3c53514e21e952ea564fcc4d4bb5f644c439ad2
abu-sani <abu.sani@byslglobal.com>
2024-10-15 16:43:10 +0600

    two service create with nest

--- sample-serverless-api-main/src/app.module.ts
+++ sample-serverless-api-main/src/app.module.ts
@@ -2,17 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite', // SQLite database file
-      autoLoadEntities: true,      // Automatically load entities
-      synchronize: true,           // Auto-sync DB schema (dev only)
-    }),
-    BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
