869f96c611325d2f1ee6c0b26d79af66f11f6a89
abu-sani <abu.sani@byslglobal.com>
2024-10-23 11:54:04 +0600

    download git project is fixed

---
+++ rabbitmq3/microservices_nestjs_rabbitmq_course-0.0.5/apps/ms-b-delivery/src/ms-b-delivery.module.ts
@@ -0,0 +1,26 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Module, DynamicModule } from '@nestjs/common';
+import { databaseProviders } from 'apps/ms-a-stock/src/ms-a-stock.database.provider';
+import { MsBDeliveryController } from './ms-b-delivery.controller';
+import { MsBDeliveryService } from './ms-b-delivery.service';
+
+@Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'delivery',
+          type: 'topic',
+        },
+      ],
+      uri: 'amqp://localhost:5672',
+    }) as DynamicModule,
+    MsBDeliveryModule,
+  ],
+  controllers: [MsBDeliveryController],
+  providers: [
+    MsBDeliveryService,
+    ...databaseProviders,
+  ],
+})
+export class MsBDeliveryModule {}
