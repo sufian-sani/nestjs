f70a6b2743c955cdd8217beccc799e0c14661c24
abu-sani <abu.sani@byslglobal.com>
2024-10-23 18:48:26 +0600

    auth service 1.0

--- mycv2/src/users/users.controller.ts
+++ mycv2/src/users/users.controller.ts
@@ -6,23 +6,47 @@ import {
     NotFoundException,
     Param,
     Patch,
-    Post,
+    Post, Session,
 } from '@nestjs/common';
 import {CreateUserDto} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto";
 import { Serialize } from '../interceptors/serialize.interceptor'
 import {UserDto} from "./dtos/user.dto";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) {}
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService,
+    ) {}
+
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        return this.usersService.findOne(session.userId);
     }
 
-    @Serialize(UserDto)
+    @Post('/signup')
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
+
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         console.log('handler is running')
