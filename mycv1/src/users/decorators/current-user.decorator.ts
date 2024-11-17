4f2768d031368ad6282681d5c744338c4dca79de
abu-sani <abu.sani@byslglobal.com>
2024-11-17 18:36:08 +0600

    check my branch

--- mycv1/src/users/decorators/current-user.decorator.ts
+++ mycv1/src/users/decorators/current-user.decorator.ts
@@ -3,6 +3,7 @@ import {createParamDecorator, ExecutionContext} from "@nestjs/common";
 export const CurrentUser = createParamDecorator(
     (data: any, context: ExecutionContext) => {
         const request = context.switchToHttp().getRequest();
-        return request.currentUser;
+        console.log(request.session.userId);
+        return 'request.currentUser';
     }
 )
