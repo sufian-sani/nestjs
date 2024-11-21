536cea163e3a21142411ccca6e274dad46cec921
abu-sani <abu.sani@byslglobal.com>
2024-11-21 17:14:27 +0600

    query builder complete

--- mycv1/src/reports/reports.service.ts
+++ mycv1/src/reports/reports.service.ts
@@ -5,11 +5,29 @@ import {Report} from "./report.entity";
 import {CreateReportDtoDto} from "./dto/create-report.dto";
 import {User} from "../users/user.entity";
 import {request} from "express";
+import {GetEstimateDto} from "./dto/get-estimate.dto";
 
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+
+    createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+        return this.repo
+            .createQueryBuilder()
+            .select('AVG(price)', 'price')
+            .where('make = :make', { make })
+            .andWhere('model = :model', { model })
+            .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+            .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+            .andWhere('year - :year BETWEEN -3 AND 3', { year })
+            .orderBy('ABS(mileage - :mileage)', 'DESC')
+            .setParameters({ mileage })
+            .limit(3)
+            .getRawOne();
+    }
+
     create(reportDto: CreateReportDtoDto, user: User) {
         const report = this.repo.create(reportDto);
         report.user = user;
