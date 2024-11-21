9c5c7d209b315f08cf6f3c3f7eff5b19396c1ed7
abu-sani <abu.sani@byslglobal.com>
2024-11-21 11:57:52 +0600

    middle ware

--- doc/doc-basic/src/cats/cats.module.ts
+++ doc/doc-basic/src/cats/cats.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { LoggerMiddleware } from '../../common/middleware/logger.middleware';
 
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+        .apply(LoggerMiddleware)
+        .forRoutes('cats');
+  }
+}
--- doc/doc-basic/src/cats/cats.service.ts
+++ doc/doc-basic/src/cats/cats.service.ts
@@ -1,26 +1,43 @@
+// import { Injectable } from '@nestjs/common';
+// import { CreateCatDto } from './dto/create-cat.dto';
+// import { UpdateCatDto } from './dto/update-cat.dto';
+
+// @Injectable()
+// export class CatsService {
+//   create(createCatDto: CreateCatDto) {
+//     return 'This action adds a new cat';
+//   }
+//
+//   findAll() {
+//     return `This action returns all cats`;
+//   }
+//
+//   findOne(id: number) {
+//     return `This action returns a #${id} cat`;
+//   }
+//
+//   update(id: number, updateCatDto: UpdateCatDto) {
+//     return `This action updates a #${id} cat`;
+//   }
+//
+//   remove(id: number) {
+//     return `This action removes a #${id} cat`;
+//   }
+// }
+
+// --------------
 import { Injectable } from '@nestjs/common';
-import { CreateCatDto } from './dto/create-cat.dto';
-import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
   }
 
-  findAll() {
-    return `This action returns all cats`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} cat`;
-  }
-
-  update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} cat`;
+  findAll(): Cat[] {
+    return this.cats;
   }
 }
